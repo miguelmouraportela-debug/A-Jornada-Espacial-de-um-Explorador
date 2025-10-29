4dc54734724b2fcc1645208e5cf05a3185d47793
miguelmouraportela-debug <miguel.moura.portela@escola.pr.gov.br>
2025-10-29 10:07:34 -0300

    M

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const avanca =document.querySelectorAll('.btn-proximo') 
 console.log(avanca)
+
+avanca.forEach(Button => {
+Button.addEventListener('click',function (){
+    const atual = document.querySelector('.ativo')
+})
+
+}) 
